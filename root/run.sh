c4f9f5031e00f05912304b556c6a4a3f833d4e10
Fabien Crespel <fabien@crespel.net>
2020-10-04 19:47:57 +0200

    Add POSTFIX_MYNETWORKS env var

--- root/run.sh
+++ root/run.sh
@@ -8,6 +8,7 @@ POSTFIX_BASIC_SPAM_PREVENTION="${POSTFIX_BASIC_SPAM_PREVENTION:-medium}"
 POSTFIX_SMTP_TLS_CLIENT="${POSTFIX_SMTP_TLS_CLIENT:-yes}"
 POSTFIX_TLS_CERTFILE="${POSTFIX_TLS_CERTFILE:-}"
 POSTFIX_TLS_KEYFILE="${POSTFIX_TLS_KEYFILE:-}"
+POSTFIX_MYNETWORKS="${POSTFIX_MYNETWORKS:-}"
 
 # Update sysconfig from environment variables
 for FILE in /etc/sysconfig/mail /etc/sysconfig/postfix; do
@@ -28,6 +29,9 @@ postconf -e smtpd_milters="$SMTPD_MILTERS"
 postconf -e non_smtpd_milters="\$smtpd_milters"
 postconf -e milter_default_action="accept"
 postconf -e internal_mail_filter_classes="bounce"
+if [ -n "$POSTFIX_MYNETWORKS" ]; then
+    postconf -e mynetworks="$POSTFIX_MYNETWORKS"
+fi
 
 # Start in foreground
 exec /usr/sbin/postfix start-fg
